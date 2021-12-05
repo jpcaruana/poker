4c3283daa3d76466eb921b006a149cb87cf07011
Jean-Philippe CARUANA <jp@caruana.fr>
2021-12-05 13:21:13 +0100

    update deps and esbuild

--- mix.exs
+++ mix.exs
@@ -33,16 +33,20 @@ defmodule Poker.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.0"},
+      {:phoenix, "~> 1.6.2"},
       {:phoenix_html, "~> 3.0"},
-      {:phoenix_live_view, "~> 0.16.4"},
+      {:phoenix_live_view, "~> 0.16.0"},
       {:phoenix_live_dashboard, "~> 0.5"},
       {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 0.5"},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
+      {:telemetry_poller, "~> 1.0"},
+      {:gettext, "~> 0.18"},
+      {:jason, "~> 1.2"},
+      {:plug_cowboy, "~> 2.5"},
       {:uuid, "~> 1.1"},
+      {:swoosh, "~> 1.3"},
+
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:floki, ">= 0.30.0", only: :test},
       {:esbuild, "~> 0.2", runtime: Mix.env() == :dev}
     ]
   end
@@ -55,7 +59,8 @@ defmodule Poker.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "cmd npm install --prefix assets"],
+      setup: ["deps.get"],
+      test: ["test"],
       "assets.deploy": ["esbuild default --minify", "phx.digest"]
     ]
   end
